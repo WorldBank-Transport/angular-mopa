ccc4b1fb93746cf0f8604dde474ad7e51ce086e3
Emil <bmachaku@gmail.com>
2016-11-14 16:02:12 +0300

    initialize project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mopaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mopaApp
+ */
+angular.module('mopaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mopaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mopaApp
+ */
+angular.module('mopaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
