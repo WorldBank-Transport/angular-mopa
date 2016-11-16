cf533bf58d5ea375e5f79641621cb75d2d81c9a2
Emil <bmachaku@gmail.com>
2016-11-16 13:42:39 +0300

    add config file

---
+++ app/scripts/services/config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name mopaApp.config
+ * @description
+ * # config
+ * Constant in the mopaApp.
+ */
+angular.module('mopaApp')
+  .constant('config', 42);
