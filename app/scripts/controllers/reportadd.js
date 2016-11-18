f799ac0be0999ce669a3d10ae8e8f01adf343e45
Emil <bmachaku@gmail.com>
2016-11-18 18:38:03 +0300

    add service group

--- app/scripts/controllers/reportadd.js
+++ app/scripts/controllers/reportadd.js
@@ -8,15 +8,18 @@
  * Controller of the mopaApp
  */
 angular.module('mopaApp')
-  .controller('ReportaddCtrl', function ($scope, $location, $http, config, report) {
+  .controller('ReportaddCtrl', function ($scope, $location, $http, $filter, config, report) {
     $scope.newReport = {};
     $scope.serviceCodes = [];
+    $scope.serviceCodesAll = [];
+    $scope.selectedServiceGroup = '';
 
     // FIXME: adding dummy coordinates
     $scope.newReport = {lat: -25.933857882269, long: 32.579494714737};
 
     $http.get(config.API_ROOT + '/services.json', {}).then(function (response){
       $scope.serviceCodes = response.data;
+      $scope.serviceCodesAll = response.data;
     });
 
     $scope.now = function (){
@@ -28,4 +31,13 @@ angular.module('mopaApp')
         $location.url('/report/' + response[0].service_request_id);
       });
     };
+
+    $scope.updateServiceGroup = function (){
+      $scope.serviceCodes = angular.copy($scope.serviceCodesAll);
+      $scope.newReport.service_code = '';
+      if ($scope.selectedServiceGroup){
+        $scope.serviceCodes = $filter('filter')($scope.serviceCodesAll, {"group": $scope.selectedServiceGroup});
+      }
+    };
+
   });
