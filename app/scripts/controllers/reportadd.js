6918b6b946c825e3579ff8823fad8582c979159f
Emil <bmachaku@gmail.com>
2016-11-18 13:06:07 +0300

    use service codesfrom the API

--- app/scripts/controllers/reportadd.js
+++ app/scripts/controllers/reportadd.js
@@ -8,14 +8,19 @@
  * Controller of the mopaApp
  */
 angular.module('mopaApp')
-  .controller('ReportaddCtrl', function ($scope, $location, report) {
+  .controller('ReportaddCtrl', function ($scope, $location, $http, config, report) {
     $scope.newReport = {};
+    $scope.serviceCodes = []
 
     // FIXME: adding dummy coordinates
     $scope.newReport = {lat: -25.933857882269, long: 32.579494714737};
 
+    $http.get(config.API_ROOT + '/services.json', {}).then(function (response){
+      $scope.serviceCodes = response.data;
+    });
+
     $scope.now = function (){
-       return new Date().toISOString();
+      return new Date().toISOString();
     };
 
     $scope.submitReport = function (){
