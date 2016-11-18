cd94afcd2a14067051296fcc809c4c9e11c93e0d
Samweli <smwltwesa6@gmail.com>
2016-11-18 16:40:18 +0300

    implemented login logic

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,35 +8,23 @@
  * Controller of the mopaApp
  */
  
-    angular
-        .module('mopaApp')
-        .controller('LoginCtrl',LoginCtrl);
- 
-    LoginCtrl.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+ angular.module('mopaApp')
+ .controller('LoginCtrl',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
 
-
-    function LoginCtrl($location, AuthenticationService, FlashService) {
-        var vm = this;
- 
-        vm.login = login;
-
-        console.log("init")
- 
-        (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })();
- 
-        function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/index.html');
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(response.data) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    $location.path('/');
                 } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
                 }
             });
         };
-    }
+    }]);
