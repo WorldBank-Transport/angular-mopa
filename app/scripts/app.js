09ebc7a30782fea076b69d53ea705bb127c3edef
Emil <bmachaku@gmail.com>
2016-11-20 18:59:08 +0300

    remove unused variable

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,7 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider, $locationProvider) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
